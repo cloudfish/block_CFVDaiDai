c29c12e31a912e76f8da35752516a05e6e0a9257
cloudfish <13962946+cloudfish@users.noreply.github.com>
2024-07-13 13:38:52 +0800

    新增识别呆哥头像的油猴脚本

--- README.md
+++ README.md
@@ -1,6 +1,29 @@
 # block_CFVDaiDai
-随缘收集雀魂吧吧宠呆呆兽的小号，并使用 uBlock Origin 自定义规则进行屏蔽。
+本项目通过两种方式屏蔽雀魂吧吧宠呆哥。目前，两种方式叠加使用，理论上（打开网页后可能有数秒延迟）可以屏蔽呆哥的几乎所有帖子（除了未知的小号发布的主题帖）。
 
-文件说明：
-- `rules.txt`: 将里面的内容粘贴到 uBlock Origin 的自定义静态规则。或者，在网络条件允许的情况下，如需要自动更新，可点击规则列表-自定义-导入，将网址 `https://raw.githubusercontent.com/cloudfish/block_CFVDaiDai/main/rules.txt` 粘贴进去。
-- `gen_tieba_ubo.sh`: 对单个账号生成屏蔽规则的 shell 脚本。使用方法：执行 `./gen_tieba_ubo.sh "目标账号贴吧主页网址"`
+方式一（黑名单）：
+
+暴力收集呆哥小号，并使用 uBlock Origin 自定义规则进行屏蔽。
+
+方式一使用说明：
+
+将 `rules.txt` 的内容粘贴到 uBlock Origin 的自定义静态规则。或者，在网络条件允许的情况下（你懂的），如需要自动更新，可点击规则列表-自定义-导入，将网址 `https://raw.githubusercontent.com/cloudfish/block_CFVDaiDai/main/rules.txt` 粘贴进去。
+
+和方式一有关的其他文件：
+
+`gen_tieba_ubo.sh`: 对单个账号生成屏蔽规则的 shell 脚本。使用方法：执行 `./gen_tieba_ubo.sh "目标账号贴吧主页网址"`
+
+方式二（启发式古户绘梨花头像识别）：
+
+使用油猴脚本识别并标记呆哥头像，然后使用 uBlock Origin 进行屏蔽。理论上可以屏蔽呆哥所有未知的小号的帖子，除非呆哥换头像逃避检测。
+
+（本人不会前端开发，所以各种复制粘贴代码做了一个勉强能用的版本。性能等方面尚有改进空间）
+
+方式二使用说明：
+
+安装油猴插件（如 ViolentMonkey）。新建一个脚本，将 `block_daidai_avatar.js` 的内容粘贴进去。然后，在 uBlock Origin 的自定义静态规则中，添加以下内容：
+
+```
+tieba.baidu.com##.l_post:has(img.dai_post)
+tieba.baidu.com##.lzl_single_post:has(img.dai_lzl)
+```
---
+++ block_daidai_avatar.js
@@ -0,0 +1,102 @@
+// ==UserScript==
+// @name 屏蔽呆哥
+// @namespace http://tampermonkey.net/
+// @version 0.1
+// @description try to take over the world!
+// @author You
+// @match https://tieba.baidu.com/*
+// @icon https://www.google.com/s2/favicons?sz=64&domain=tieba.baidu.com
+// @grant GM_xmlhttpRequest
+// ==/UserScript==
+
+
+(function() {
+    "use strict";
+    const targetNode = document.querySelector(".l_container");
+    const config = { subtree: true, childList: true };
+    const callback = (mutationList, observer) => {
+    let avatar = targetNode.getElementsByTagName("img");
+    for (let index = 0; index < avatar.length; index++) {
+        let item = avatar[index];
+        if (item.classList.contains("not_dai")||item.classList.contains("dai_post")||item.classList.contains("dai_lzl")) {
+            continue;
+        }
+        GM_xmlhttpRequest({
+            method: "GET",
+            responseType: "blob",
+            url: item.src,
+            onload: function(response) {
+                if (response.status === 200) {
+                    new Promise((resolve, reject) => {
+                        let reader = new FileReader();
+                        reader.addEventListener("load", () => {
+                            let img = new Image();
+                            img.setAttribute("crossOrigin", "anonymous");
+                            img.src = reader.result;
+                            img.addEventListener("load", () => resolve(img));
+                        });
+                        reader.readAsDataURL(response.response);
+                    }).then((img) => getImageData(item, img));
+                }
+            },
+        });
+    }
+
+    function getImageData(item, img) {
+        let canvas = document.createElement("canvas");
+        canvas.setAttribute("width", `${item.width}px`);
+        canvas.setAttribute("height", `${item.height}px`);
+        var ctx = canvas.getContext("2d");
+        ctx.drawImage(img, 0, 0, item.width, item.height);
+
+
+        let imgData = ctx.getImageData(
+            item.width / 4,
+            item.height / 4,
+            item.width / 2,
+            item.height / 2
+        );
+
+        let map = new Map();
+        for (let i = 0; i < imgData.data.length; i += 4) {
+            let r = imgData.data[i];
+            let g = imgData.data[i + 1];
+            let b = imgData.data[i + 2];
+            // let a
+            let rgb = `${r},${g},${b}`;
+            if (!map.has(rgb)) {
+                map.set(rgb, 1);
+            } else {
+                map.set(rgb, map.get(rgb) + 1);
+            }
+        }
+        // 寻找最大值
+        let maxValue = 0;
+        let color = "";
+        map.forEach((value, key) => {
+            if (value > maxValue) {
+                maxValue = value;
+                color = key;
+            }
+        });
+        console.log(item.src);
+        console.log(color);
+        let a = color.split(",");
+        if (a[0]>=30&&a[0]<=36&&a[1]>=37&&a[1]<=45&&a[2]>=93&&a[2]<=113) {
+            if(item.hasAttribute("username")) {
+                item.classList.add("dai_post");
+            } else {
+                item.classList.add("dai_lzl");
+            }
+            item.style.display = "none";
+        } else {
+            item.classList.add("not_dai");
+        }
+    }
+    };
+    // Create an observer instance linked to the callback function
+    const observer = new MutationObserver(callback);
+
+    // Start observing the target node for configured mutations
+    observer.observe(targetNode, config);
+})();
