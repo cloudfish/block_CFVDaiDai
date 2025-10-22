9cd804dd044bfd1a5275c3cde66b04f445d60d43
cloudfish <13962946+cloudfish@users.noreply.github.com>
2025-10-22 20:15:30 +0800

    优化楼中楼选择器性能，并屏蔽所有在楼中楼里at被屏蔽的账号的用户

--- gen_tieba_ubo.sh
+++ gen_tieba_ubo.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-a=$(echo $1 | cut -d '?' -f 2 | cut -d '&' -f 1)
-echo "tieba.baidu.com##.j_thread_list:has(a.j_user_card[href*=\"${a}\"])"
-echo "tieba.baidu.com##.l_post:has(a.p_author_name[href*=\"${a}\"])"
-echo "tieba.baidu.com##.lzl_single_post:has(a.j_user_card[href*=\"${a}\"])"
+a=$(echo $1 | cut -d '?' -f 2 | cut -d '&' -f 1 | cut  -d '=' -f 2)
+echo "tieba.baidu.com##.j_thread_list:has(a.j_user_card[href*=\"id=${a}\"])"
+echo "tieba.baidu.com##.l_post:has(a.p_author_name[href*=\"id=${a}\"])"
+echo "tieba.baidu.com##.lzl_single_post:has(a.j_user_card img[src$=\"${a}\"])"
+echo "tieba.baidu.com##.lzl_single_post:has(.lzl_content_main a.at[portrait=\"${a}\"])"
